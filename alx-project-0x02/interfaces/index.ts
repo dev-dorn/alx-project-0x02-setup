e291b7511f91fbe320a7107fb752ea05b527b2d0
dev-dorn <berrymundia@gmail.com>
2025-10-04 18:31:33 +0300

    added a button

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -40,3 +40,11 @@ export interface PostModalProps {
 
     
 }
+
+export interface ButtonProps {
+    size: "small" | "medium" | "large";
+    shape : "rounded-sm " | "rounded-md" | "rounded-full";
+    children : React.ReactNode;
+    onClick?: () => void;
+
+}
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,7 +1,14 @@
-export default function About () {
-    return (
-        <>
-        help us chose the right experience for you
-        </>
-    )
+import React from "react";
+import Button from "@/components/common/Button";
+
+export default function AboutPage() {
+  return (
+    <div className="flex flex-col items-center gap-4 mt-10">
+      <h1 className="text-2xl font-bold">About Page</h1>
+
+      <Button size="small" shape="rounded-sm">Small Button</Button>
+      <Button size="medium" shape="rounded-md">Medium Button</Button>
+      <Button size="large" shape="rounded-full">Large Button</Button>
+    </div>
+  );
 }
